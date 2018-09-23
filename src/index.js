860629690719cae37f734c24789776b7cfdc708b
kotcher1 <annashkt@gmail.com>
2018-09-24 01:41:00 +0300

    spich

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(pref = []) {
+  let spich = 0;
+  const tri = [];
+  for (let count = 0; count < pref.length; count++) {
+    if (!(tri.indexOf(count) + 1)) {
+      if ((count + 1) === pref[count]) {
+        tri.push(count)
+      } else {
+        const pew = pref[pref[pref[count] - 1] - 1] - 1;
+        if (pew === count) {
+          spich++;
+          tri.push(count);
+          tri.push(pref[count] - 1);
+          tri.push(pref[pref[count] - 1] - 1)
+        };
+      };
+    };
+  };
+  return spich
 };
